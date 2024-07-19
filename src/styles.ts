a7540ce9d8f18ac64e09cdb9797a57516fe31989
xxxAlvaDevxxx <dtamjob@google.com>
2024-07-19 02:13:00 -0400

    feat: styles.ts this is for the future

---
+++ src/styles.ts
@@ -0,0 +1,19 @@
+import { StyleDeclaration } from "xxx-element/types";
+
+export const COLORS_BASE = {
+  "purple-heart": {
+    "50": "#f3f1ff",
+    "100": "#ebe5ff",
+    "200": "#d9ceff",
+    "300": "#bea6ff",
+    "400": "#9f75ff",
+    "500": "#843dff",
+    "600": "#7916ff",
+    "700": "#6b04fd",
+    "800": "#5a03d5",
+    "900": "#4b05ad",
+    "950": "#2c0076",
+  },
+};
+
+/* COLORS_BASE["purple-heart"][100] */
