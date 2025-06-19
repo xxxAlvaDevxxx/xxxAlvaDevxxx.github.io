9274f8c8ac2d18408e94defcefc0128d28c38c22
xxxAlvaDevxxx <dtamjob@google.com>
2025-06-19 09:52:06 -0400

    feat: add Blog component to Main and update children structure

--- src/components/Main.ts
+++ src/components/Main.ts
@@ -2,16 +2,22 @@ import { $main } from "xxx-element/elementHTML";
 import About from "./main/About";
 import Projects from "./main/Projects";
 import Contact from "./main/Contact";
+import Blog from "./main/Blog";
 import Header from "./Header";
 import MainFeature from "../interfaces/MainFeature";
 
 class Main extends $main {
   $about = About;
   $projects = Projects;
+  $blog = Blog;
   $contact = Contact;
   constructor() {
     super();
-    this.addChildren(this.$about, this.$projects, this.$contact);
+    this.addChildren(
+      this.$about,
+      this.$projects /* ,this.$blog */,
+      this.$contact
+    );
   }
   addChildren(...children: MainFeature[]): this {
     super.addChildren(...children);
